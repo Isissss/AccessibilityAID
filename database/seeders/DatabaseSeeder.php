b3cfb43282b5a7a5f34bc2cd66c917af91e4e68e
Isis <1036029@hr.nl>
2022-11-08 18:01:00 +0100

    V1 of User story 8. Add to the rapport page and use markdown helper.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Challenge;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $challenge = Challenge::create([
+            'name' => 'besturing',
+            'slug' => 'besturing',
+            'description' => 'voer dit en dit uit',
+            'goal' => 'Dit is het doel',
+            'active' => true
+        ]);
+
+        $challenge->tips()->create(['content' => 'Dit is om te laten zien hoe een tip **eruit ziet**. Dit kunt u hier vinden: [Test](https://www.youtube.com/)']);
     }
 }
