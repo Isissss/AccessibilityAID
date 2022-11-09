fc4bd0ebe0e1e1b3acc846cdd0300f7b78505bfa
Isis <1036029@hr.nl>
2022-11-09 12:19:04 +0100

    Resolve merge conflict

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
+Route::resource('/challenge', ChallengeController::class);
 
-Route::get('/challenge/{challenge:slug}', [ChallengeController::class, 'show'])->name('challenge.show');;
-Route::get('/challenge/{challenge:slug}/finished', [FinishedChallengeController::class, 'show'])->name('completed-challenge.show');
-Route::post('/finished/{completed_challenge}', [FinishedChallengeController::class, 'update'])->name('completed-challenge.update');
+Route::get('/challenge/{challenge:slug}', [ChallengeController::class, 'show']);
 
+Route::get('/challenge/{challenge:slug}/finished', [FinishedChallengeController::class, 'show']);
 
 Auth::routes();
 
