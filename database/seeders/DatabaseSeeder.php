69c35ceba41dc34191bfed3871e5ce386433a139
christian 2 <christian.van.der.helm@gmail.com>
2022-11-30 20:58:04 +0100

    Fixed DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,37 +24,37 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         //password = Gebruiker123
-//        User::create([
-//            'name' => 'Gebruiker',
-//            'email' => 'gebruiker@gebruiker.com',
-//            'password' => '$2y$10$GfSAw2yFvw//2033XKedWOCIWmAWriVZAHYDsL.WsrMZrMKnb0QmW',
-//        ]);
-//
-//        $challenge = Challenge::create([
-//            'slug' => 'contrast',
-//            'name' => 'Contrast',
-//            'description' => 'Opdracht met contrast problemen',
-//            'goal' => 'Zoek de breedte filter op de website en klik er op',
-//        ]);
-//
-//        Challenge::create([
-//            'slug' => 'besturing',
-//            'name' => 'besturing',
-//            'description' => 'Opdracht met met tab navigatie',
-//            'goal' => 'Navigeer naar element ... op de pagina',
-//        ]);
+        User::create([
+            'name' => 'Gebruiker',
+            'email' => 'gebruiker@gebruiker.com',
+            'password' => '$2y$10$GfSAw2yFvw//2033XKedWOCIWmAWriVZAHYDsL.WsrMZrMKnb0QmW',
+        ]);
+
+        $challenge = Challenge::create([
+            'slug' => 'contrast',
+            'name' => 'Contrast',
+            'description' => 'Opdracht met contrast problemen',
+            'goal' => 'Zoek de breedte filter op de website en klik er op',
+        ]);
+
+        Challenge::create([
+            'slug' => 'besturing',
+            'name' => 'besturing',
+            'description' => 'Opdracht met met tab navigatie',
+            'goal' => 'Navigeer naar element ... op de pagina',
+        ]);
 
 
-        Challenge::find(1)->tips()->create(['content' => 'Dit is om te laten zien hoe een tip **eruit ziet**. Dit kunt u hier vinden: [Test](https://www.youtube.com/)']);
-        Challenge::find(1)->tips()->create(['content' => 'Op ** deze website ** check je hoe het zit met de contrast op uw website . Deze kleurentest vind je [hier](https://www.youtube.com/)']);
-//
-//
-//        Challenge::create([
-//            'slug' => 'test',
-//            'name' => 'Test',
-//            'description' => 'Test opdracht',
-//            'goal' => 'Doe iets',
-//        ]);
+        $challenge->tips()->create(['content' => 'Dit is om te laten zien hoe een tip **eruit ziet**. Dit kunt u hier vinden: [Test](https://www.youtube.com/)']);
+        $challenge->tips()->create(['content' => 'Op ** deze website ** check je hoe het zit met de contrast op uw website . Deze kleurentest vind je [hier](https://www.youtube.com/)']);
+
+
+        Challenge::create([
+            'slug' => 'test',
+            'name' => 'Test',
+            'description' => 'Test opdracht',
+            'goal' => 'Doe iets',
+        ]);
 
     }
 }
