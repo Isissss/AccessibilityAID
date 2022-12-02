0fd8309e35e403dc911b6822be4d11d37439417b
Christian van der Helm <79118248+1011352@users.noreply.github.com>
2022-12-02 11:20:00 +0100

    Update edit.blade.php

--- resources/views/adminTips/edit.blade.php
+++ resources/views/adminTips/edit.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
 
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Builds</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-
-<body>
 <div class="container mt-2">
     <div class="row">
         <div class="col-lg-12 margin-tb">
@@ -42,6 +33,5 @@
         </div>
     </form>
 </div>
-</body>
 
 @endsection
