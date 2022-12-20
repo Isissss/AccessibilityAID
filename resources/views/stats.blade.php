7aa9a4dfb30d7f555b8b2e2aae96bb51f3bd5088
1011352 <1011352@hr.nl>
2022-12-20 11:44:31 +0100

    spell check

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -13,12 +13,12 @@
     <h1 class="text-center display-3">Accessibility</h1>
     <div class="row">
         <div class="col-sm">
-            <p class="blockquote">De reden waarom toegankelijkheid zo belangrijk is is omdat er zijn veel meer mensen dan we denken die beperkingen hebben</p>
+            <p class="blockquote">De reden waarom toegankelijkheid zo belangrijk is, is omdat er veel meer mensen zijn dan we denken die beperkingen hebben</p>
             <p class="blockquote">In de statistiek kan je zien dat er totaal 6.7 miljoen Nederlanders zijn die een beperking hebben.</p>
             <p class="blockquote">Dit is rond <strong>38%</strong> van de Nederlandse populatie</p>
-            <p class="blockquote">Dit betekent dat als je site niet toegankelijk is dan kan <strong>1/3 van je klanten</strong> jouw producten niet kopen.</p>
+            <p class="blockquote">Dit betekent, dat als je site niet toegankelijk is dan kan <strong>1/3 van jouw klanten</strong> je producten niet kan kopen.</p>
             <p class="blockquote">Dit veroorzaakt minder inkomsten en groei voor je webshop</p>
-            <p class="blockquote">Maar met deze digitale handboek kan je uitdaging doen die je tips kan geven om <strong>jouw</strong> website te verbeteren.</p>
+            <p class="blockquote">Maar met dit digitale handboek kan je de uitdaging aangaan en de tips gebruiken om <strong>jouw</strong> website te verbeteren.</p>
         </div>
         <div class="col-sm">
             <img src="https://media.discordapp.net/attachments/396548841786179585/1054486482489966592/output-onlinepngtools.png" alt="Responsive Image" class="img-thumbnail rounded">
