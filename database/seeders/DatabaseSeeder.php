ff71b7bb7b2a0952588185b15878643f0ecec084
1011352 <1011352@hr.nl>
2023-01-16 20:00:33 +0100

    page which shows stats

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Challenge;
+use App\Models\Tip;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -50,10 +51,60 @@ class DatabaseSeeder extends Seeder
             'description' => 'Deze opdracht simuleert hoe iemand die geen muis kan gebruiken een webshop beleeft. Als u de opdracht start krijgt u een pagina te zien waarop u door middel van de TAB knop op uw toetsenbord de pagina moet navigeren een een specifiek element moet selecteren',
             'goal' => 'Navigeer naar het element: Categorie 5',
         ]);
+        Tip::create([
+            'challenge_id' => '1',
+            'content' => 'Wat je op moet letten. Niet alleen donker kleuren gebruiken en vergelijkbare kleuren. Dit maakt het moeilijk voor mensen die kleurenblind zijn om woorden te zien. De beste manier om dit te vermijden is kleuren met veel contrast te gebruiken.',
+            'wordpress'=>'0'
+        ]);
+        Tip::create([
+            'content' => 'Op **deze website** check je hoe het zit met de contrast op uw website . Deze kleurentest vind je [hier](https://webaim.org/resources/contrastchecker/)',
+            'challenge_id' => '1'
+        ]);
+        Tip::create([
+            'content' => 'Op **deze website** krijg je 8 goed tips die je website kleurenblind vriendelijk maakt. Deze site vind je [hier](https://www.audioeye.com/post/8-ways-to-design-a-color-blind-friendly-website/)',
+            'challenge_id' => '1'
+        ]);
+        Tip::create([
+            'challenge_id' => '1',
+            'content' => 'Op **deze website** kan je een extensie downloaden voor wordpress die checkt of je site accessible is. Deze extensie vind je [hier](https://wordpress.org/plugins/accessibility-checker/)',
+            'wordpress'=>'1'
+        ]);
+        Tip::create([
+            'challenge_id' => '1',
+            'content' => 'Op **deze website** kan je een extensie downloaden die je website gewoon in een keer accessible maakt. Dit extensie fixed contrast, font en veel meer. Deze extensie vind je [hier](https://wordpress.org/plugins/pojo-accessibility/)',
+            'wordpress'=>'1'
+        ]);
+        Tip::create([
+            'challenge_id' => '2',
+            'content' => 'Wat je op moet letten. Meeste mensen met een beperking gebruiken **TAB** om door de website te gaan dus het is handig om een visual te geven van waar je bent zodat ze weten wanneer ze ENTER moeten klikken',
+            'wordpress'=>'0'
+        ]);
+        Tip::create([
+            'challenge_id' => '2',
+            'content' => 'Op **deze website** krijg je tips over navigatie en het verbeteren van invoervelden. Deze website vind je [hier](https://www.nngroup.com/articles/keyboard-accessibility/)',
+            'wordpress'=>'0'
+        ]);
+        Tip::create([
+            'challenge_id' => '2',
+            'content' => 'Op **deze website** krijg je tips over CSS en HTML om je website te verbeteren. Deze website vind je [hier](https://webdesign.tutsplus.com/articles/keyboard-accessibility-tips-using-html-and-css--cms-31966)',
+            'wordpress'=>'0'
+        ]);
+        Tip::create([
+            'challenge_id' => '2',
+            'content' => 'Op **deze website** krijg je tips over navigatie en het verbeteren van invoervelden. Deze website vind je [hier](https://www.nngroup.com/articles/keyboard-accessibility/)',
+            'wordpress'=>'0'
+        ]);
+        Tip::create([
+            'challenge_id' => '2',
+            'content' => 'Op **deze website** krijg je de beste extensies om keyboard toegankelijkheid in uw website te integreren . Deze extensies vind je [hier](https://wordpress.org/plugins/tags/keyboard-navigation/)',
+            'wordpress'=>'1'
+        ]);
+        Tip::create([
+            'challenge_id' => '2',
+            'content' => 'Op **deze website** krijg je de beste extensies om keyboard toegankelijkheid in uw website te integreren . Deze extensies vind je [hier](https://wordpress.org/plugins/tags/keyboard-navigation/)',
+            'wordpress'=>'1'
+        ]);
 
-
-        $challenge->tips()->create(['content' => 'Dit is om te laten zien hoe een tip **eruit ziet**. Dit kunt u hier vinden: [Test](https://www.youtube.com/)']);
-        $challenge->tips()->create(['content' => 'Op ** deze website ** check je hoe het zit met de contrast op uw website . Deze kleurentest vind je [hier](https://www.youtube.com/)']);
 
     }
 }
